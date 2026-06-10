d0146f9df760fd101cf731ae2eeccedae177f8bc
Mabel Mendoza <mabelanaimendozalopez@gmail.com>
2026-06-10 17:18:18 -0600

    Clases

---
+++ Modelos/DatosApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskNotes_MonitoreoTareas.Sub_Forms;
+
+namespace TaskNotes_MonitoreoTareas.Modelos
+{
+    public static class DatosApp
+    {
+        
+    }
+}
